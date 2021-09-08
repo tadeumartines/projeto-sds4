35b47e456d2c9870ef5f08daabc80d495fa63a74
Tadeu Martines <tadeumartines@yahoo.com.br>
2021-09-08 20:48:44 -0300

    Basic statics components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function DataTable() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/tadeumartines" target="_blank" rel="noreferrer">Tadeu Martines</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/tadeumartines" target="_blank" rel="noreferrer">@tadeumartines.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default DataTable;
